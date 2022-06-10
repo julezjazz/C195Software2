a4d07591c664492e4e9abc9e6e260559dd78d597
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-10 13:53:54 -0400

    Renamed files and set up skeleton for LogIn page

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import helper.JDBC;
 import javafx.application.Application;
@@ -12,10 +12,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/LogIn.fxml"));
+        primaryStage.setTitle("Log In");
+        primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.show();
+
     }
 
 
