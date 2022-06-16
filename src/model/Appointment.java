f953aa2227ca5ca81b61386b66349d671cd0f882
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-16 13:01:24 -0400

    Added constructor, setters, and getters for Appointment

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -12,7 +12,97 @@ public class Appointment {
     private int userId;
     private int contactId;
 
+    public Appointment(int appointmentId, String title, String description, String location, String type,
+                       String startDateTime, String endDateTime, int customerId, int userId, int contactId) {
+        this.appointmentId = appointmentId;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.type = type;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.customerId = customerId;
+        this.userId = userId;
+        this.contactId = contactId;
+    }
 
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setStartDateTime(String startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public void setEndDateTime(String endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getStartDateTime() {
+        return startDateTime;
+    }
+
+    public String getEndDateTime() {
+        return endDateTime;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getContactId() {
+        return contactId;
+    }
 }
