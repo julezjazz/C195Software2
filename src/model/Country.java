ab8d4a59390407d61e69d756fa73455670c6e5c9
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-16 09:52:30 -0400

    Moved classes from main to model, and created getters, setters, and constructor for Country.

---
+++ src/model/Country.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Country {
+    private int countryId;
+    private String countryName;
+
+    public Country(int countryId, String countryName) {
+        this.countryId = countryId;
+        this.countryName = countryName;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+}
