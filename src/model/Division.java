71eb314acc1e615e3326e19ec0168bc9b465c70f
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-16 10:04:18 -0400

    Added attributes, getters, setters, and constructor for Division

--- src/model/Division.java
+++ src/model/Division.java
@@ -1,4 +1,28 @@
 package model;
 
 public class Division {
+
+    public int divisionId;
+    public String divisionName;
+
+    public Division(int divisionId, String divisionName){
+        this.divisionId = divisionId;
+        this.divisionName = divisionName;
+    }
+
+    public void setDivisionId(int divisionId) {
+        this.divisionId = divisionId;
+    }
+
+    public void setDivisionName(String divisionName) {
+        this.divisionName = divisionName;
+    }
+
+    public int getDivisionId() {
+        return divisionId;
+    }
+
+    public String getDivisionName() {
+        return divisionName;
+    }
 }
