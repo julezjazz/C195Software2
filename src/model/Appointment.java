f023f7883224d4b9016abcd116395efab4d90bbb
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-16 12:50:55 -0400

    Added attributes to Appointment

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,6 +1,18 @@
 package model;
 
 public class Appointment {
+    private int appointmentId;
+    private String title;
+    private String description;
+    private String location;
+    private String type;
+    private String startDateTime;
+    private String endDateTime;
+    private int customerId;
+    private int userId;
+    private int contactId;
+
+
 
 
 }
