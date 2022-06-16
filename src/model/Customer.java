bf04a1a7eea6548440a6ac89bfc0872214a82e22
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-16 12:21:02 -0400

    Added attributes to Customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -2,5 +2,16 @@ package model;
 
 public class Customer {
 
+    private int customerId;
+    private String customerName;
+    private String address;
+    private String postalCode;
+    private int divisionId;
+    private int countryId;
+
+
+
+
+
 
 }
