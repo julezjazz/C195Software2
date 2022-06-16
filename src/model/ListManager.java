f891fce1bd0b2210bf480707cdbd377c03a49893
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-16 14:57:49 -0400

    Added lists to ListManager

--- src/model/ListManager.java
+++ src/model/ListManager.java
@@ -1,4 +1,13 @@
 package model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class ListManager {
+    public ObservableList<Customer> allCustomers = FXCollections.observableArrayList();
+    public ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
+    public ObservableList<Country> allCountries = FXCollections.observableArrayList();
+    public ObservableList<Division> allDivisions = FXCollections.observableArrayList();
+    public ObservableList<User> allUsers = FXCollections.observableArrayList();
+    public ObservableList<Contact> allContacts = FXCollections.observableArrayList();
 }
