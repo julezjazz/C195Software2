087d8d877b8bac5b2b388c6e701f8f60b9d918bc
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-16 12:37:26 -0400

    Added constructor, setters, and getters for Customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -6,12 +6,74 @@ public class Customer {
     private String customerName;
     private String address;
     private String postalCode;
+    private String phone;
     private int divisionId;
     private int countryId;
 
+    public void Customer(int customerId, String customerName, String address, String postalCode, String phone,
+                         int divisionId, int countryId){
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.phone = phone;
+        this.divisionId = divisionId;
+        this.countryId = countryId;
+    }
 
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
 
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setDivisionId(int divisionId) {
+        this.divisionId = divisionId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getDivisionId() {
+        return divisionId;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
 }
