9e2b5c0ed931a6bc8857ca43bc82d41068165695
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-17 13:49:12 -0400

    Added javadoc comments for getters, setters, and class.

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -1,5 +1,5 @@
 package model;
-
+/** Represents a contact. */
 public class Contact {
     private int contactId;
     private String contactName;
@@ -10,27 +10,27 @@ public class Contact {
         this.contactName = contactName;
         this.email = email;
     }
-
+    /** Setter for contact ID. */
     public void setContactId(int contactId) {
         this.contactId = contactId;
     }
-
+    /** Setter for contact name. */
     public void setContactName(String contactName) {
         this.contactName = contactName;
     }
-
+    /** Setter for contact email. */
     public void setEmail(String email) {
         this.email = email;
     }
-
+    /** Getter for contact ID. */
     public int getContactId() {
         return contactId;
     }
-
+    /** Getter for contact name. */
     public String getContactName() {
         return contactName;
     }
-
+    /** Getter for contact email. */
     public String getEmail() {
         return email;
     }
