83da734331e12a383db28734f1fe5989ab1d1acb
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-17 14:12:33 -0400

    Added javadoc comments for class and lists

--- src/model/ListManager.java
+++ src/model/ListManager.java
@@ -2,12 +2,18 @@ package model;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
+/** Creates and manages lists for other class objects.*/
 public class ListManager {
+    /** Creates list of all objects representing customers. */
     public ObservableList<Customer> allCustomers = FXCollections.observableArrayList();
+    /** Creates list of all objects representing appointments. */
     public ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
+    /** Creates list of all objects representing countries. */
     public ObservableList<Country> allCountries = FXCollections.observableArrayList();
+    /** Creates list of all objects representing first divisions of countries. */
     public ObservableList<Division> allDivisions = FXCollections.observableArrayList();
+    /** Creates list of all objects representing users. */
     public ObservableList<User> allUsers = FXCollections.observableArrayList();
+    /** Creates list of all objects representing contacts for appointments. */
     public ObservableList<Contact> allContacts = FXCollections.observableArrayList();
 }
