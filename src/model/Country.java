66a16528935ae501edb48f279b5b042a0da52d43
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-17 13:53:57 -0400

    Created javadoc comments for class, getters, and setters

--- src/model/Country.java
+++ src/model/Country.java
@@ -1,5 +1,5 @@
 package model;
-
+/** Represents a country. */
 public class Country {
     private int countryId;
     private String countryName;
@@ -8,19 +8,19 @@ public class Country {
         this.countryId = countryId;
         this.countryName = countryName;
     }
-
+    /** Setter for country ID. */
     public void setCountryId(int countryId) {
         this.countryId = countryId;
     }
-
+    /** Setter for name of country. */
     public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
-
+    /** Getter for country ID. */
     public int getCountryId() {
         return countryId;
     }
-
+    /** Getter for name of country.  */
     public String getCountryName() {
         return countryName;
     }
