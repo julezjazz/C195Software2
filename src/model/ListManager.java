b561f3437b9feac3a70e46d7c3f61ca9eb125144
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-17 17:42:37 -0400

    Changed javadoc comments to be more accurate.

--- src/model/ListManager.java
+++ src/model/ListManager.java
@@ -4,16 +4,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 /** Creates and manages lists for other class objects.*/
 public class ListManager {
-    /** Creates list of all objects representing customers. */
+    /** Creates list for all objects representing customers. */
     public ObservableList<Customer> allCustomers = FXCollections.observableArrayList();
-    /** Creates list of all objects representing appointments. */
+    /** Creates list for all objects representing appointments. */
     public ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
-    /** Creates list of all objects representing countries. */
+    /** Creates list for all objects representing countries. */
     public ObservableList<Country> allCountries = FXCollections.observableArrayList();
-    /** Creates list of all objects representing first divisions of countries. */
+    /** Creates list for all objects representing first divisions of countries. */
     public ObservableList<Division> allDivisions = FXCollections.observableArrayList();
-    /** Creates list of all objects representing users. */
+    /** Creates list for all objects representing users. */
     public ObservableList<User> allUsers = FXCollections.observableArrayList();
-    /** Creates list of all objects representing contacts for appointments. */
+    /** Creates list for all objects representing contacts for appointments. */
     public ObservableList<Contact> allContacts = FXCollections.observableArrayList();
 }
