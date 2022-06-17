45bb96afdd4fb75850bcac3ee3248d1fafb8f664
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-17 14:01:48 -0400

    Added javadoc comments for class, getters, and setters for Division.

--- src/model/Division.java
+++ src/model/Division.java
@@ -1,5 +1,5 @@
 package model;
-
+/** Represents a first level division of a country. */
 public class Division {
 
     private int divisionId;
@@ -9,19 +9,19 @@ public class Division {
         this.divisionId = divisionId;
         this.divisionName = divisionName;
     }
-
+    /** Setter for division ID */
     public void setDivisionId(int divisionId) {
         this.divisionId = divisionId;
     }
-
+    /** Setter for name of division. */
     public void setDivisionName(String divisionName) {
         this.divisionName = divisionName;
     }
-
+    /** Getter for division ID. */
     public int getDivisionId() {
         return divisionId;
     }
-
+    /** Getter for name of division. */
     public String getDivisionName() {
         return divisionName;
     }
