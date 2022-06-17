1c408fdd4522dd5e8cbdd1eec65c0bdf715e8eba
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-17 10:05:57 -0400

    Added javadoc comments for constructor, getters, and setters.

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,5 +1,5 @@
 package model;
-
+/** Represents an appointment. */
 public class Appointment {
     private int appointmentId;
     private String title;
@@ -11,7 +11,7 @@ public class Appointment {
     private int customerId;
     private int userId;
     private int contactId;
-
+/** Class constructor. */
     public Appointment(int appointmentId, String title, String description, String location, String type,
                        String startDateTime, String endDateTime, int customerId, int userId, int contactId) {
         this.appointmentId = appointmentId;
@@ -25,83 +25,83 @@ public class Appointment {
         this.userId = userId;
         this.contactId = contactId;
     }
-
+/** Setter for appointment ID. */
     public void setAppointmentId(int appointmentId) {
         this.appointmentId = appointmentId;
     }
-
+/** Setter for appointment title. */
     public void setTitle(String title) {
         this.title = title;
     }
-
+    /** Setter for appointment description. */
     public void setDescription(String description) {
         this.description = description;
     }
-
+    /** Setter for appointment location. */
     public void setLocation(String location) {
         this.location = location;
     }
-
+    /** Setter for appointment type. */
     public void setType(String type) {
         this.type = type;
     }
-
+    /** Setter for appointment start date and start time. */
     public void setStartDateTime(String startDateTime) {
         this.startDateTime = startDateTime;
     }
-
+    /** Setter for appointment end date and end time. */
     public void setEndDateTime(String endDateTime) {
         this.endDateTime = endDateTime;
     }
-
+    /** Setter for customer ID. */
     public void setCustomerId(int customerId) {
         this.customerId = customerId;
     }
-
+    /** Setter for user ID. */
     public void setUserId(int userId) {
         this.userId = userId;
     }
-
+    /** Setter for appointment contact ID. */
     public void setContactId(int contactId) {
         this.contactId = contactId;
     }
-
+    /** Getter for appointment ID. */
     public int getAppointmentId() {
         return appointmentId;
     }
-
+    /** Getter for appointment title. */
     public String getTitle() {
         return title;
     }
-
+    /** Getter for appointment description. */
     public String getDescription() {
         return description;
     }
-
+    /** Getter for appointment location. */
     public String getLocation() {
         return location;
     }
-
+    /** Getter for appointment type. */
     public String getType() {
         return type;
     }
-
+    /** Getter for appointment start time and start date. */
     public String getStartDateTime() {
         return startDateTime;
     }
-
+    /** Getter for appointment end time and end date. */
     public String getEndDateTime() {
         return endDateTime;
     }
-
+    /** Getter for customer ID. */
     public int getCustomerId() {
         return customerId;
     }
-
+    /** Getter for user ID. */
     public int getUserId() {
         return userId;
     }
-
+    /** Getter for appointment contact ID. */
     public int getContactId() {
         return contactId;
     }
