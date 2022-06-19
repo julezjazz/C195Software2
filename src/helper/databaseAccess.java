e286d67f7e751603bd8bc8f9e89f880c80b78931
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-19 16:06:21 -0400

    Added code to retrieve user data from database

--- src/helper/databaseAccess.java
+++ src/helper/databaseAccess.java
@@ -4,10 +4,32 @@ import helper.JDBC;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.ListManager;
+import model.User;
+
+import java.sql.*;
 
 public class databaseAccess {
 
+    String sql = "select * from users";
 
+    PreparedStatement ps;
 
+    {
+        try {
+            ps = JDBC.getConnection().prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int userId = rs.getInt("User_ID");
+                String userName = rs.getString("User_Name");
+                String password = rs.getString("Password");
+                User newUser = new User(userId, userName, password);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 }
 
