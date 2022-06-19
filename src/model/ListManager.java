470e70e8a3c7625b5ee0b8ad0d1ab3f5d3fb1a62
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-19 09:49:01 -0400

    Added addCustomer, addAppointment, getAllCustomers, and getAllAppointments methods

--- src/model/ListManager.java
+++ src/model/ListManager.java
@@ -16,4 +16,15 @@ public class ListManager {
     public ObservableList<User> allUsers = FXCollections.observableArrayList();
     /** List for all objects representing contacts for appointments. */
     public ObservableList<Contact> allContacts = FXCollections.observableArrayList();
+
+    /** Adds new customer to list of customers. */
+    public void addCustomer(Customer newCustomer) {allCustomers.add(newCustomer);}
+    /** Adds new appointment to list of appointments. */
+    public void addAppointment(Appointment newAppointment) {allAppointments.add(newAppointment);}
+
+    /** Outputs list of all customers */
+    public ObservableList<Customer> getAllCustomers(){return allCustomers;}
+    /**Outputs list of all appointments. */
+    public ObservableList<Appointment> getAllAppointments(){return allAppointments;}
 }
+
