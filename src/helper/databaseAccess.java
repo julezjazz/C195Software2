ec201c2bc21e256f707ce5b256c259021835f9ec
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-19 15:10:32 -0400

    Created a databaseAccess class for getting data from the database

--- src/helper/databaseAccess.java
+++ src/helper/databaseAccess.java
@@ -1,4 +1,11 @@
 package helper;
 
+import helper.JDBC;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.ListManager;
+
 public class databaseAccess {
+
+
 }
