fe087bd1d71241e76a80530ee0e104d73b1a24c2
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-19 15:34:14 -0400

    Recreated database access class, as it now seems that I do need it.

---
+++ src/helper/databaseAccess.java
@@ -0,0 +1,13 @@
+package helper;
+
+import helper.JDBC;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.ListManager;
+
+public class databaseAccess {
+
+
+
+}
+
