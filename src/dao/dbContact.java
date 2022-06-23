8e22963dc929e597222ab98d2cbca54f0f485e18
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-23 11:29:44 -0400

    Moved allContacts list from ListManager to dbContact and added code for retrieving contact info from the database, making contact objects and adding them to the list.

--- src/dao/dbContact.java
+++ src/dao/dbContact.java
@@ -1,4 +1,40 @@
 package dao;
 
+import helper.JDBC;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Contact;
+import model.ListManager;
+import model.User;
+
+import java.sql.*;
+
 public class dbContact {
+
+    /** List for all objects representing contacts for appointments. */
+    public ObservableList<Contact> allContacts = FXCollections.observableArrayList();
+
+    String sql = "select * from contacts";
+
+    PreparedStatement ps;
+
+    {
+        try {
+            ps = JDBC.getConnection().prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int contactId = rs.getInt("Contact_ID");
+                String contactName = rs.getString("Contact_Name");
+                String email = rs.getString("Email");
+
+                Contact newContact = new Contact(contactId, contactName, email);
+                allContacts.add(newContact);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 }
--- src/model/ListManager.java
+++ src/model/ListManager.java
@@ -14,8 +14,7 @@ public class ListManager {
     public ObservableList<Division> allDivisions = FXCollections.observableArrayList();
     /** List for all objects representing users. */
     public static ObservableList<User> allUsers = FXCollections.observableArrayList();
-    /** List for all objects representing contacts for appointments. */
-    public ObservableList<Contact> allContacts = FXCollections.observableArrayList();
+
 
     /** Adds new customer to list of customers. */
     public void addCustomer(Customer newCustomer) {allCustomers.add(newCustomer);}
