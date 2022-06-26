b6cc8f0c20c98bafc73a764e0a4c278da056a880
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-06-26 13:52:00 -0400

    Deleted non-functioning method from UserDao.

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -33,42 +33,5 @@ public class UserDao {
 
         }
     }
-
-    public static boolean verifyLogIn (String username, String password) {
-        //String sql = "select * from users" ;
-        //tried "select * from users where User_Name = ?" and ps.setInt, but it didn't work.
-
-
-        //PreparedStatement ps;
-
-        {
-            try {
-                PreparedStatement ps = JDBC.getConnection().prepareStatement("select * from users where User_Name = '?'");
-                ps.setString(1, username);
-                //ps = JDBC.getConnection().prepareStatement(sql);
-                 //ps = JDBC.getConnection().prepareStatement("select * from users where User_Name = ?")
-                 //ps.setInt(1, username);
-
-                ResultSet rs = ps.executeQuery();
-                while (rs.next()) {
-                    if (password == rs.getString("Password")){
-                        System.out.println("SUCCESS!");
-                        return true;
-                    }
-                    //if (username == rs.getString("User_Name")) {
-                      //  if (password == rs.getString("Password")) {
-                        //    System.out.println("SUCCESS!");
-                          //  return true;
-                        }
-
-
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-
-        }
-        System.out.println("FAIL");
-        return false;
-    }
 }
 
