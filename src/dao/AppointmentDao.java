492f12b4f166ffb8b7fd943c290b12acd65ca93a
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-07-14 13:26:27 -0400

    Removed unnecessary code

--- src/dao/AppointmentDao.java
+++ src/dao/AppointmentDao.java
@@ -4,7 +4,6 @@ import helper.JDBC;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.Appointment;
-import model.Customer;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
