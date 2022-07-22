9e44197b736bb642b265baa86aa6eb344559ef44
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-07-22 16:43:22 -0400

    Update modify appointment form to include time combo boxes and datepicker and add variables, textfields, etc to controller

--- src/controller/ModifyAppointment.java
+++ src/controller/ModifyAppointment.java
@@ -6,12 +6,57 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class ModifyAppointment implements Initializable {
+
+    public TextField appointmentIdTF;
+    public TextField titleTF;
+    public TextField descriptionTF;
+    public TextField locationTF;
+    public ComboBox contactCB;
+    public TextField typeTF;
+    public DatePicker startDateDP;
+    public ComboBox startHourCB;
+    public ComboBox startMinuteCB;
+    public DatePicker endDateDP;
+    public ComboBox endHourCB;
+    public ComboBox endMinuteCB;
+    public TextField customerIdTF;
+    public TextField userIdTF;
+
+    public String title;
+    public String description;
+    public String location;
+    public String contactName;
+    public String type;
+    public String startDate;
+    public String endDate;
+    public String startHour;
+    public String endHour;
+    public String startMinute;
+    public String endMinute;
+    public String startTime;
+    public String endTime;
+    public Timestamp startDateTime;
+    public Timestamp endDateTime;
+    public String createdBy;
+    public int customerId;
+    public int userId;
+    public int contactId;
+
+    ZoneId utcZI = ZoneId.of("UTC");
+    ZoneId userZI = ZoneId.systemDefault();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public void onSaveReturnBtn(ActionEvent actionEvent) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../view/Home.fxml"));
