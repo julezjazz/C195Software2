16db19e8c67194631fcf1c44acfcc6f8736369ac
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-07-28 12:53:58 -0400

    Delete all changes from previous commit

--- src/dao/AppointmentDao.java
+++ src/dao/AppointmentDao.java
@@ -9,7 +9,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 public class AppointmentDao {
 
@@ -40,17 +39,15 @@ public class AppointmentDao {
                     int userId = rs.getInt("User_ID");
                     String contactName = rs.getString("Contact_Name");
 
-                   /* String startDate = startDateTime.toLocalDateTime().toLocalDate().toString();
+                    String startDate = startDateTime.toLocalDateTime().toLocalDate().toString();
                     String startTime = startDateTime.toLocalDateTime().toLocalTime().toString();
 
                     String endDate = endDateTime.toLocalDateTime().toLocalDate().toString();
-                    String endTime = endDateTime.toLocalDateTime().toLocalTime().toString(); */
+                    String endTime = endDateTime.toLocalDateTime().toLocalTime().toString();
 
-                    LocalDateTime startDateTimeDT = startDateTime.toLocalDateTime();
-                    LocalDateTime endDateTimeDT = endDateTime.toLocalDateTime();
 
                     Appointment newAppointment = new Appointment(appointmentId, title, description, location, type,
-                            startDateTimeDT, endDateTimeDT, customerId, userId, contactName);
+                            startDate, startTime, endDate, endTime, customerId, userId, contactName);
                     allAppointments.add(newAppointment);
                 }
 
