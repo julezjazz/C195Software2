36a92ffc79c2aa8fddb7c27d46362e94d8330cd1
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-08-13 12:56:42 -0400

    Set populateContactSchedule method to clear before adding appointments to avoid duplicate or innacurate results

--- src/helper/ListMaker.java
+++ src/helper/ListMaker.java
@@ -3,11 +3,11 @@ package helper;
 import dao.AppointmentDao;
 import javafx.collections.ObservableList;
 import model.Appointment;
-import model.Contact;
 import model.ListManager;
 
 public class ListMaker {
     public static ObservableList<Appointment> populateContactSchedule (int contactId) {
+        ListManager.contactSchedule.clear();
         for(Appointment appointment : AppointmentDao.populateAppointmentList()){
             if (appointment.getContactId() == contactId) {
                 ListManager.contactSchedule.add(appointment);
