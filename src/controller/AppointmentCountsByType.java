4fcd29788c4d3cc3030bef55b5517f9530c07fdc
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-08-13 14:23:00 -0400

    Set up combo box to populate and count report text to clear

--- src/controller/AppointmentCountsByType.java
+++ src/controller/AppointmentCountsByType.java
@@ -1,4 +1,20 @@
 package controller;
 
-public class AppointmentCountsByType {
+import helper.ListMaker;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AppointmentCountsByType implements Initializable {
+    public ComboBox appointmentTypeCB;
+    public Text countReport;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        countReport.setText("");
+        appointmentTypeCB.setItems(ListMaker.populateAppointmentTypes());
+    }
 }
