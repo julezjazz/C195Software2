840593b1b7a82eb6906efba63b241bc8b7771d04
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-08-13 11:05:39 -0400

    Set customers and appointments overview buttons to navigate to those pages

--- src/controller/ReportsMenu.java
+++ src/controller/ReportsMenu.java
@@ -1,4 +1,29 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ReportsMenu {
+    public void onAppointmentsBtn (ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/Appointments.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 1100, 600);
+        stage.setTitle("Appointments");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void onCustomersBtn (ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/Customers.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 1000, 600);
+        stage.setTitle("Customers");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
