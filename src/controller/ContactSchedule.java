8108851b77e72cda7512277d27e79cb5290187a3
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-08-13 11:57:19 -0400

    Set reports button to navigate to reports menu page

--- src/controller/ContactSchedule.java
+++ src/controller/ContactSchedule.java
@@ -1,6 +1,12 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,5 +16,13 @@ public class ContactSchedule implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+    public void onReportsBtn(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/ReportsMenu.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 280, 244);
+        stage.setTitle("Reports Menu");
+        stage.setScene(scene);
+        stage.show();
     }
 }
