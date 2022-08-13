06941fb6a4429e8546405a22bd7e5ad5cf2c45fb
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-08-13 14:45:12 -0400

    Add button for going back to Reports Menu and set button to navigate to that page

--- src/controller/AppointmentCountsByType.java
+++ src/controller/AppointmentCountsByType.java
@@ -1,9 +1,15 @@
 package controller;
 
 import helper.ListMaker;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,5 +22,13 @@ public class AppointmentCountsByType implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         countReport.setText("");
         appointmentTypeCB.setItems(ListMaker.populateAppointmentTypes());
+    }
+    public void onReportsBtn(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/ReportsMenu.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 280, 244);
+        stage.setTitle("Reports Menu");
+        stage.setScene(scene);
+        stage.show();
     }
 }
