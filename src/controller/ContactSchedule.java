dda0c0b5a7c9a86d75e5e3c63efe50cb7367ab5a
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-08-13 12:11:41 -0400

    Have combo box populate with list of contact names

--- src/controller/ContactSchedule.java
+++ src/controller/ContactSchedule.java
@@ -6,16 +6,30 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+import model.ListManager;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ContactSchedule implements Initializable {
 
+    public ComboBox contactCB;
+    public TableView scheduleTable;
+    public TableColumn appointmentIdCol;
+    public TableColumn titleCol;
+    public TableColumn descriptionCol;
+    public TableColumn typeCol;
+    public TableColumn startCol;
+    public TableColumn endCol;
+    public TableColumn customerIdCol;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        contactCB.setItems(ListManager.allContactNames);
     }
     public void onReportsBtn(ActionEvent actionEvent) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../view/ReportsMenu.fxml"));
