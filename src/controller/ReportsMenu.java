49e0cd1c6a52ad4b9df84e20227de734a3f3c20a
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-08-13 11:57:47 -0400

    Set contact schedule button to navigate to contact schedule page

--- src/controller/ReportsMenu.java
+++ src/controller/ReportsMenu.java
@@ -10,6 +10,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ReportsMenu {
+
+    public void onContactSchedBtn (ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/ContactSchedule.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 1065, 600);
+        stage.setTitle("Contact Schedule");
+        stage.setScene(scene);
+        stage.show();
+    }
     public void onAppointmentsBtn (ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("../view/Appointments.fxml"));
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
