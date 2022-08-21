257950ce919fb52d649fbe43565ee12696b94145
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-08-20 22:08:42 -0400

    Clean up code

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,11 +12,9 @@ import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-
 /** This class creates an app that displays screens. */
 public class Main extends Application {
     public static ResourceBundle rb;
-
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -24,12 +22,8 @@ public class Main extends Application {
         primaryStage.setTitle("Log In");
         primaryStage.setScene(new Scene(root, 550, 400));
         primaryStage.show();
-
     }
-
-
     public static void main(String[] args) {
-        //Locale.setDefault(Locale.CANADA);
         try {rb = ResourceBundle.getBundle("main/Lang", Locale.getDefault());}
         catch (MissingResourceException e){
             System.out.println("No resource bundle available for " + Locale.getDefault().getDisplayCountry());
@@ -40,20 +34,6 @@ public class Main extends Application {
         CountryDao.populateCountryLists();
         DivisionDao.populateDivisionList();
 
-
-        // DELETE Test Data
-       /* for (User user : ListManager.allUsers) {
-            System.out.println(user.getUserName());
-            System.out.println(user.getPassword());
-        } */
-
-        /* for (Contact contact : ListManager.allContacts){
-            System.out.println(contact.getContactName());
-        } */
-        // DELETE Test Data
-       /* for (Country country : ListManager.allCountries){
-            System.out.println(country.getCountryName());
-        }*/
         launch(args);
         JDBC.closeConnection();
     }
