27fd8d4933f2c852f81fbbeaf358b515395ab653
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-08-28 10:14:17 -0400

    Clean up code and add javadoc comments

--- src/controller/Customers.java
+++ src/controller/Customers.java
@@ -13,9 +13,16 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import dao.CustomerDao;
 import model.Customer;
+
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+/**
+ * A class for controlling <code>../view/Customers.fxml</code>.
+ * @author Julez Hudson
+ */
 public class Customers implements Initializable {
     public TableView customersTable;
     public TableColumn customerCol;
@@ -28,6 +35,11 @@ public class Customers implements Initializable {
 
     public static Customer selectedCustomer;
 
+    /**
+     * Fills the customer table view with a list of all customers and sets the display text to blank"
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         messageText.setText(" ");
@@ -39,7 +51,13 @@ public class Customers implements Initializable {
         postalCol.setCellValueFactory(new PropertyValueFactory<>("postalCode"));
         phoneCol.setCellValueFactory(new PropertyValueFactory<>("phone"));
     }
-    public void onAddCustomerBtn(ActionEvent actionEvent) throws Exception {
+
+    /**
+     * Navigates to the Add Customer page.
+     * @param actionEvent Clicking the add customer button.
+     * @throws IOException In case of input or output exception.
+     */
+    public void onAddCustomerBtn(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("../view/AddCustomer.fxml"));
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root, 900, 600);
@@ -47,7 +65,14 @@ public class Customers implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
-    public void onModCustomerBtn(ActionEvent actionEvent) throws Exception {
+
+    /**
+     * Navigates to the modify customer page. If no customer is selected, this method displays text alerting the user
+     * to select a customer to modify and returns without navigating to another page.
+     * @param actionEvent Clicking the modify customer button.
+     * @throws IOException In case of an input or output exception.
+     */
+    public void onModCustomerBtn(ActionEvent actionEvent) throws IOException {
         selectedCustomer = (Customer) customersTable.getSelectionModel().getSelectedItem();
 
         if(selectedCustomer == null){
@@ -62,7 +87,15 @@ public class Customers implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
-    public void onDelCustomerBtn(ActionEvent actionEvent) throws Exception {
+
+    /**
+     * Deletes selected customer from the Customers table of the database and updates the table view to reflect the
+     * change. If no customer is selected when the delete button is pressed, this method sets the display text to alert
+     * the user that they need to select a customer to delete. Otherwise, this method sets the display text to notify
+     * the user that customer has been deleted.
+     * @throws SQLException In case of a sql exception.
+     */
+    public void onDelCustomerBtn() throws SQLException {
         selectedCustomer = (Customer) customersTable.getSelectionModel().getSelectedItem();
 
         if(selectedCustomer == null){
@@ -85,7 +118,13 @@ public class Customers implements Initializable {
         postalCol.setCellValueFactory(new PropertyValueFactory<>("postalCode"));
         phoneCol.setCellValueFactory(new PropertyValueFactory<>("phone"));
     }
-    public void onReportsBtn(ActionEvent actionEvent) throws Exception {
+
+    /**
+     * Navigates to the reports menu.
+     * @param actionEvent Clicking the reports menu button.
+     * @throws IOException In case of input or output exception.
+     */
+    public void onReportsBtn(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("../view/ReportsMenu.fxml"));
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root, 280, 244);
