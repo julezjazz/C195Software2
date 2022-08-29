7a7b757caa170b68fbedc2f2152f4d361b3dd558
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-08-28 20:24:15 -0400

    Add javadoc comments

--- src/dao/AppointmentDao.java
+++ src/dao/AppointmentDao.java
@@ -11,10 +11,19 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+/**
+ * A class for working with the Appointment table in the database.
+ * @author Julez Hudson
+ */
 public class AppointmentDao {
 
     public static ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
 
+    /**
+     * Retrieves all rows from the Appointments table of the database and creates an Appointment object for each then
+     * adds each to the list for all appointments.
+     * @return The list of all appointments.
+     */
     public static ObservableList<Appointment> populateAppointmentList(){
         String sql = "select * from appointments";
 
@@ -56,6 +65,20 @@ public class AppointmentDao {
         return null;
     }
 
+    /**
+     * Inserts a new row into the Appointments table of the database.
+     * @param title The appointment title.
+     * @param description The appointment description.
+     * @param location The location where the appointment will be held.
+     * @param type The type of appointment that will be held.
+     * @param start The date and time that the appointment will begin.
+     * @param end The date and time that the appointment will end.
+     * @param createdBy The user who add the appointment to the table.
+     * @param customerId The ID number of the customer who will attend the appointment.
+     * @param userId The ID number of the user who will attend the appointment.
+     * @param contactId The ID number of the contact who will attend the appointment.
+     * @throws SQLException In case of a sql exception.
+     */
     public static void insert(String title, String description, String location,
                               String type, Timestamp start, Timestamp end, String createdBy, int customerId,
                               int userId, int contactId) throws SQLException {
@@ -76,6 +99,21 @@ public class AppointmentDao {
         ps.execute();
     }
 
+    /**
+     * Updates a row in the Appointments table of the database.
+     * @param title The appointment title.
+     * @param description The appointment description.
+     * @param location The location where the appointment will be held.
+     * @param type The type of appointment that will be held.
+     * @param start The date and time that the appointment will begin.
+     * @param end The date and time that the appointment will end.
+     * @param updatedBy The user who modified the appointment in the database.
+     * @param customerId The ID number of the customer who will attend the appointment.
+     * @param userId The ID number of the user who will attend the appointment.
+     * @param contactId The ID number of the contact who will attend the appointment.
+     * @param appointmentId The ID of the appointment that is being changed in the database.
+     * @throws SQLException In case of a sql exception.
+     */
     public static void update(String title, String description, String location, String type, Timestamp start,
                               Timestamp end, String updatedBy, int customerId, int userId, int contactId,
                               int appointmentId)
@@ -100,6 +138,11 @@ public class AppointmentDao {
         ps.execute();
     }
 
+    /**
+     * Deletes a row from the Appointments table of the database
+     * @param appointmentId The ID number of the appointment row that is being deleted.
+     * @throws SQLException In case of a sql exception.
+     */
     public static void delete(int appointmentId) throws SQLException {
         String sql = "delete from appointments where appointment_id = ?";
 
