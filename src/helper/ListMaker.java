e561d28e87196e354bea8b1bd368c774dec70fc5
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-09-03 10:38:04 -0400

    Clean up code and add javadoc comments

--- src/helper/ListMaker.java
+++ src/helper/ListMaker.java
@@ -6,11 +6,18 @@ import javafx.collections.ObservableList;
 import model.Appointment;
 import model.Customer;
 import model.Division;
-
 import java.time.LocalDate;
 
+/**
+ * A class for populating lists.
+ * @author Julez Hudson
+ */
 public class ListMaker {
 
+    /**
+     * Clears the list of all customer names and populates it from the list of all customers.
+     * @return the list of all customer names
+     */
     public static ObservableList<String> populateAllCustomerNames(){
         ListManager.allCustomerNames.clear();
         for (Customer customer : CustomerDao.populateCustomerList()){
@@ -18,6 +25,13 @@ public class ListMaker {
         }
         return ListManager.allCustomerNames;
     }
+
+    /**
+     * Clears then populates the appointments by customer list. Appointments are added based on the appointments by
+     * month list and based on the customer ID provided.
+     * @param customerId the ID number of the customer on which the appointments list being populated is based
+     * @return the list of appointments by customer
+     */
     public static ObservableList<Appointment> populateAppointmentsByCustomers (int customerId) {
         ListManager.appointmentsByCustomer.clear();
         for (Appointment appointment : ListMaker.populateAppointmentsByMonth()){
@@ -28,6 +42,12 @@ public class ListMaker {
         return ListManager.appointmentsByCustomer;
     }
 
+    /**
+     * Clears then populates the contact schedule list of appointments. Appointments are added to the list based on the
+     * appointments by month list and the contact ID provided.
+     * @param contactId the ID number of the contact on which the appointments being added to the list are based
+     * @return the contact schedule list of appointments
+     */
     public static ObservableList<Appointment> populateContactSchedule (int contactId) {
         ListManager.contactSchedule.clear();
         for(Appointment appointment : populateAppointmentsByMonth()){
@@ -38,6 +58,11 @@ public class ListMaker {
         return ListManager.contactSchedule;
     }
 
+    /**
+     * Clears then populates the all appointment types list. Appointment types are added to the list based on the
+     * list of all appointments.
+     * @return the list of all appointment types
+     */
     public static ObservableList<String> populateAppointmentTypes(){
         ListManager.allAppointmentTypes.clear();
         String appointmentType;
@@ -51,6 +76,11 @@ public class ListMaker {
         return ListManager.allAppointmentTypes;
     }
 
+    /**
+     * Clears then populates the appointments by month list. Adds appointments from the list of all appointments if they
+     * have start dates within a 30 day range starting from the current date and ending 29 days after the current date.
+     * @return the appointments by month list
+     */
     public static ObservableList<Appointment> populateAppointmentsByMonth(){
         LocalDate curDate = LocalDate.now();
         LocalDate oneMonth = curDate.plusDays(29);
@@ -64,6 +94,12 @@ public class ListMaker {
         }
         return ListManager.appointmentsByMonth;
     }
+
+    /**
+     * Clears then populates the appointments by week list. Adds appointments from the list of all appointments if they
+     * have start dates within a 7 day range starting from the current date and ending 6 days after the current date.
+     * @return the list of appointments by week
+     */
     public static ObservableList<Appointment> populateAppointmentsByWeek() {
         LocalDate curDate = LocalDate.now();
         LocalDate oneWeek = curDate.plusDays(6);
@@ -78,6 +114,12 @@ public class ListMaker {
         return ListManager.appointmentsByWeek;
     }
 
+    /**
+     * Clears then populates the divisions by select country list. Divisions are added from the list of all divisions,
+     * but only divisions with a country ID matching the one provided are added to the list.
+     * @param countryId the ID number of the country on which the divisions are based
+     * @return the list of divisions by select country
+     */
     public static ObservableList<String> populateDivisionsBySelectCountry(int countryId) {
         ListManager.divisionsBySelectCountry.clear();
         for (Division division : ListManager.allDivisions) {
