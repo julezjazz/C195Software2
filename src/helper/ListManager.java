7ef9a68725886a3899275b9bde4ada27fc3a17ed
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-09-18 21:51:24 -0400

    Make a list of months for the purpose of populating the new combo box in appointment acounts by type

--- src/helper/ListManager.java
+++ src/helper/ListManager.java
@@ -4,6 +4,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.*;
 
+import java.time.Month;
+
 /**
  *  A class for storing lists.
  *  @author Julez Hudson
@@ -33,5 +35,9 @@ public class ListManager {
     public static ObservableList<String> hours = FXCollections.observableArrayList("00", "01", "02", "04", "05",
             "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23");
     public static ObservableList<String> minutes = FXCollections.observableArrayList("00", "15", "30", "45");
+
+    public static ObservableList<Month> months = FXCollections.observableArrayList(Month.JANUARY, Month.FEBRUARY,
+            Month.MARCH, Month.APRIL, Month.MAY, Month.JUNE, Month.JULY, Month.AUGUST, Month.SEPTEMBER, Month.OCTOBER,
+            Month.NOVEMBER, Month.DECEMBER);
 }
 
